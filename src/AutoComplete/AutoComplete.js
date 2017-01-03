ee6c18ed3207c36f9df3ef0fee5548f998e38396
aromero <aromero@digitalguardian.com>
2017-01-03 08:25:06 -0700

    add suggestion dropdown

--- src/AutoComplete/AutoComplete.js
+++ src/AutoComplete/AutoComplete.js
@@ -1,21 +1,66 @@
+import axios from 'axios';
+import debounce from 'lodash.debounce';
 import React, { Component } from 'react';
 import './AutoComplete.css';
 
 class AutoComplete extends Component {
   constructor(props) {
     super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.state = {
-      value: ''
-    };
+    this.handleInputSearchChange = this.handleInputSearchChange.bind(this);
+    this.handleClickListItem = this.handleClickListItem.bind(this);
+    this.getAutoCompleteResults = this.getAutoCompleteResults.bind(this);
+    this.state = {autoCompleteData: []}
+    this.getAutoCompleteResults = debounce(this.getAutoCompleteResults, 300);
   }
 
-  handleChange(e) {
-    this.props.onChange(e.target.value)
+  /**
+   * get the value for the input
+   * @param  {Object} e event object passed in
+   */
+  handleInputSearchChange(e) {
+    let value = e.target.value;
+    
+    if (e.target.value.length > 0) {
+      this.getAutoCompleteResults(e.target.value)
+      this.props.onChangedInputValue(value);
+    } else {
+      this.state = {autoCompleteData: []}
+      this.props.onChangedInputValue(value);
+    }
+  }
+
+  /**
+   * when we click a list item call the prop onChange which will send an api call to get recipes
+   * @param  {String} text item which was clicked
+   */
+  handleClickListItem(text) {
+    this.props.onChangedInputValue(text);
+    this.props.onSelectedItem(text)
+    this.state = {autoCompleteData: []}
+  }
+
+  /**
+   * Make api call after 300 ms debouce to get auto complete results.  After 200 set autoCompleteData value to return.
+   * @param  {String} value string to set as query parameter
+   */
+  getAutoCompleteResults(value) {
+    console.log('hey');
+    axios.get(`https://api.nutritionix.com/v2/autocomplete?q=${value}&appId=be48f72d&appKey=36843f47de3c76347879e12f49cbfcf4`)
+      .then(response => {
+        this.setState({autoCompleteData: response.data})
+      })
+      .catch(response => {
+        console.log(response);
+      })
   }
 
   render() {
-    const value = this.props.value;
+    let borderTopStyle = {borderTop: '1px solid lightgray'};
+
+    const listItems = this.state.autoCompleteData.map((data) =>
+      <li className="autocomplete__list-item" onClick={(event) => this.handleClickListItem(data.text)} key={data.id}>{data.text}</li>
+    );
+
     return (
       <span>
         <span className="autocomplete__icon">⚲</span>
@@ -24,9 +69,12 @@ class AutoComplete extends Component {
           placeholder="search"
           type="search"
           autoFocus
-          value={value}
-          onChange={this.handleChange}
+          value={this.props.value}
+          onChange={this.handleInputSearchChange}
         />
+        <ul style={(this.state.autoCompleteData.length > 0) ? borderTopStyle : {}} className="autocomplete__list">
+          {listItems}
+        </ul> 
       </span>
     )
   }
--- src/Recommendation/Recommendation.js
+++ src/Recommendation/Recommendation.js
@@ -1,51 +1,23 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import './Recommendation.css';
 
 class Recommendation extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      recommendationData: ''
-    };
   };
 
-  sendRequest(food) {
-    var me;
-    var config = {
-      headers: {
-        'Access-Control-Allow-Origin': '*'
-      }
-    };
-
-    return axios.get(`https://api.edamam.com/search?q=${food}&app_id=ecb5988e&app_key=f60f52e1598b9838fa31de996441a797&from=0&to=3&calories=gte%20591,%20lte%20722&health=alcohol-free`, {}, config)
-      .then(response => {
-        this.setState({
-          recommendationData: response.data
-        });
-      })
-  }
-
-  componentWillReceiveProps(nextProps) {
-    // You don't have to do this check first, but it can help prevent an unneeded render
-    if (nextProps.input !== this.state.recommendationData) {
-      console.log(nextProps.input);
-      this.sendRequest(nextProps.input);
-    }
-  }
-
 	render() {
-    if (this.props.input && this.state.recommendationData.hits) {
+    if (this.props.value && this.props.data.hits) {
       return (
       	<div className="recommendation__card-container">
       		<div className="recommendation__heading-container">
-		      	<h2 className="recommendation__heading-title">{this.state.recommendationData.hits[0].recipe.label}</h2>
+		      	<h2 className="recommendation__heading-title">{this.props.data.hits[0].recipe.label}</h2>
 	      	</div>
-	      	<img className="recommendation__image-round-border" src={this.state.recommendationData.hits[0].recipe.image} />
-	      	<p>{this.state.recommendationData.hits[0].recipe.ingredientLines}</p>
+	      	<img className="recommendation__image-round-border" src={this.props.data.hits[0].recipe.image} />
+	      	<p>{this.props.data.hits[0].recipe.ingredientLines}</p>
       	</div>
       );
-    } else if (this.props.input && !this.state.recommendationData.hits) {
+    } else if (this.props.value && !this.props.data.hits) {
     	return <div>Loading..</div>
     }
 
