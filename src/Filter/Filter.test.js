f2bd9ff0e0d7c60c33db743b43f6a0e93e50b670
aromero <aromero@digitalguardian.com>
2017-01-19 22:55:17 -0700

    added test

---
+++ src/Filter/Filter.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Filter from './Filter';
+import {shallow} from 'enzyme';
+
+let sum = (x, y) => x + y; 
+
+it('sums numbers', () => {
+  expect(sum(1, 2)).toEqual(3);
+  expect(sum(2, 2)).toEqual(4);
+});
+
+test('isFilterItemSelected()', () => {
+  const component = shallow(
+    <Filter show={true}></Filter>
+  );
+
+  console.log(Filter);
+
+})
