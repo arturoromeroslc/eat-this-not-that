9fb4b43f0af2a36f3dbbdcbd6b915528fe13a387
Arturo Romero <arturo@Arturos-MacBook-Pro.local>
2017-07-30 12:27:01 -0700

    update to React 16, FIIBBBBERRRRR

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
 
+global.requestAnimationFrame = function(callback) {
+  setTimeout(callback);
+};
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
