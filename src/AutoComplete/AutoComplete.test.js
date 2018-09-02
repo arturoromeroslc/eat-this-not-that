c65378a2ed4046f7dcc544cca9aa72d2169f38ec
Arturo Romero <artromero801@gmail.com>
2018-09-02 02:44:02 -0700

    update readme

--- src/AutoComplete/AutoComplete.test.js
+++
@@ -1,9 +0,0 @@
-import { describe } from 'pm2'
-
-global.requestAnimationFrame = function(callback) {
-  setTimeout(callback)
-}
-
-describe('AutoComplete snapshot test', () => {
-  expect(1 + 1).toBe(2)
-})
