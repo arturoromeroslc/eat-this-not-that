d838de4abc502690a5036eeb12986b35e9370be2
Arturo Romero <artromero801@gmail.com>
2018-09-04 23:15:18 -0700

    add href
    add height
    
    remove unused vars

--- src/FoodCard/FoodCard.js
+++ src/FoodCard/FoodCard.js
@@ -2,19 +2,16 @@ import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import shortid from 'shortid'
 import { withStyles } from '@material-ui/core/styles'
-import classnames from 'classnames'
 import Card from '@material-ui/core/Card'
 import CardHeader from '@material-ui/core/CardHeader'
 import CardMedia from '@material-ui/core/CardMedia'
 import CardContent from '@material-ui/core/CardContent'
 import CardActions from '@material-ui/core/CardActions'
-import Collapse from '@material-ui/core/Collapse'
 import IconButton from '@material-ui/core/IconButton'
 import Typography from '@material-ui/core/Typography'
 import red from '@material-ui/core/colors/red'
 import FavoriteIcon from '@material-ui/icons/Favorite'
 import ShareIcon from '@material-ui/icons/Share'
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import MoreVertIcon from '@material-ui/icons/MoreVert'
 
 import breakpoints from '../utils/breakpoints'
@@ -29,6 +26,12 @@ const styles = theme => ({
     [breakpoints.desktop]: {
       width: '30%',
     },
+  },
+  cardHeader: {
+    '&:hover': {
+      cursor: 'pointer',
+    },
+    height: '64px',
   },
   media: {
     height: 0,
@@ -56,25 +59,26 @@ const styles = theme => ({
 })
 
 class FoodCard extends PureComponent {
-  state = { expanded: false }
-
   handleExpandClick = () => {
     this.setState(state => ({ expanded: !state.expanded }))
   }
 
   render() {
-    const { classes, ingredientLines, label, image, calories } = this.props
+    const { classes, ingredientLines, label, image, calories, url } = this.props
 
     return (
       <Card className={classes.card}>
         <CardHeader
+          className={classes.cardHeader}
+          onClick={() => {
+            window.location = url
+          }}
           action={
-            <IconButton>
+            <IconButton onClick={e => e.preventDefault()}>
               <MoreVertIcon />
             </IconButton>
           }
           title={label}
-          subheader="September 14, 2016"
         />
         <CardMedia className={classes.media} image={image} />
         <CardContent>
@@ -85,38 +89,17 @@ class FoodCard extends PureComponent {
           </Typography>
         </CardContent>
         <CardActions className={classes.actions} disableActionSpacing>
-          <IconButton aria-label="Add to favorites">
+          <IconButton
+            onClick={e => e.preventDefault()}
+            aria-label="Add to favorites"
+          >
             <FavoriteIcon />
           </IconButton>
-          <IconButton aria-label="Share">
+          <IconButton onClick={e => e.preventDefault()} aria-label="Share">
             <ShareIcon />
           </IconButton>
           {calories} cal
-          <IconButton
-            className={classnames(classes.expand, {
-              [classes.expandOpen]: this.state.expanded,
-            })}
-            onClick={this.handleExpandClick}
-            aria-expanded={this.state.expanded}
-            aria-label="Show more"
-          >
-            <ExpandMoreIcon />
-          </IconButton>
         </CardActions>
-        <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
-          <CardContent>
-            <Typography paragraph variant="body2">
-              Method:
-            </Typography>
-            <Typography paragraph>
-              Heat 1/2 cup of the broth in a pot until simmering, add saffron
-              and set aside for 10 minutes.
-            </Typography>
-            <Typography paragraph>
-              Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet
-            </Typography>
-          </CardContent>
-        </Collapse>
       </Card>
     )
   }
@@ -128,6 +111,7 @@ FoodCard.propTypes = {
   label: PropTypes.string,
   image: PropTypes.string,
   calories: PropTypes.string,
+  url: PropTypes.string,
 }
 
 export default withStyles(styles)(FoodCard)
--- src/RecipeList/RecipeList.js
+++ src/RecipeList/RecipeList.js
@@ -3,26 +3,29 @@ import PropTypes from 'prop-types'
 import Typography from '@material-ui/core/Typography'
 import FoodCard from '../FoodCard/FoodCard'
 
-import './List.css'
+import './RecipeList.css'
 
-export default class List extends Component {
+export default class RecipeList extends Component {
   render() {
     const { data } = this.props
 
     if (data.length > 0) {
       return (
         <ul className="flex">
-          {data.map(({ id, label, image, ingredientLines, calories }, i) => (
-            <FoodCard
-              key={id}
-              index={i}
-              label={label}
-              image={image}
-              ingredientLines={ingredientLines}
-              cardClicked={this.cardClick}
-              calories={calories}
-            />
-          ))}
+          {data.map(
+            ({ id, label, image, ingredientLines, calories, url }, i) => (
+              <FoodCard
+                key={id}
+                index={i}
+                label={label}
+                image={image}
+                url={url}
+                ingredientLines={ingredientLines}
+                cardClicked={this.cardClick}
+                calories={calories}
+              />
+            ),
+          )}
         </ul>
       )
     } else if (data.length === 0) {
@@ -38,11 +41,11 @@ export default class List extends Component {
   }
 }
 
-List.defaultProps = {
+RecipeList.defaultProps = {
   data: [],
 }
 
-List.propTypes = {
+RecipeList.propTypes = {
   data: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string,
