bfc99cfcd75ab40b516943594379f67edb893944
Arturo Romero <artromero801@gmail.com>
2019-05-23 00:03:50 -0700

    remove index
    add prettier
    
    prettier to devDeps
    
    add lambda
    
    add apiGateway
    
    clean up cors file
    
    add auth to endpoint
    
    clean

--- packages/infrastructure/index.ts
+++ packages/infrastructure/index.ts
@@ -1,22 +1,60 @@
 #!/usr/bin/env node
-import cdk = require('@aws-cdk/cdk');
-import s3 = require('@aws-cdk/aws-s3');
-import iam = require('@aws-cdk/aws-iam')
+import apigateway = require('@aws-cdk/aws-apigateway')
+import cdk = require('@aws-cdk/cdk')
+import lambda = require('@aws-cdk/aws-lambda')
 
-class myStaticWebsiteStack extends cdk.Stack {
-    constructor(parent: cdk.App, name: string, props: cdk.StackProps) {
-        super(parent, name, props)
+const { addCorsOptions } = require('./add-cors')
+const { cognito } = require('./cognito/cognito_arn')
 
-        const bucket = new s3.Bucket(this, 'myFirstBucket');
-        bucket.addToResourcePolicy(new iam.PolicyStatement()
-            .addActions('s3:GetObject')
-            .addResources(bucket.arnForObjects('file.txt'))
-            .addAccountRootPrincipal());
-    }
+export class LambdaFunctionStack extends cdk.Stack {
+  constructor(app: cdk.App, id: string) {
+    super(app, id)
+
+    const helloLambda = new lambda.Function(this, 'HelloHandler', {
+      code: new lambda.AssetCode('lambda'),
+      handler: 'hello.handler',
+      runtime: lambda.Runtime.NodeJS810,
+    })
+
+    const alias = new lambda.Alias(this, 'alias', {
+      aliasName: 'hello-world',
+      version: new lambda.Version(this, 'version', {
+        lambda: helloLambda,
+      }),
+    })
+
+    //create the api Service
+    const apiService = new apigateway.RestApi(this, 'helloworld-api', {
+      restApiName: 'HelloWorld Service',
+      description: 'This service calls HelloLambda.',
+    })
+
+    //create a root api gateway resource
+    const hello = apiService.root.addResource('hello')
+
+    //apigateway with lambda integration
+    const helloApiGatewayLambdaIntegration = new apigateway.LambdaIntegration(
+      alias,
+    )
+
+    const authorizer = new apigateway.CfnAuthorizer(this, 'HelloAuthorizer', {
+      authType: apigateway.AuthorizationType.Cognito,
+      providerArns: [cognito.default.arn],
+      name: 'HelloAuthorizer',
+      restApiId: apiService.restApiId,
+      identitySource: 'method.request.header.Authorization',
+      type: apigateway.AuthorizationType.Cognito,
+    })
+
+    //add a post with the lambda integration
+    hello.addMethod('post', helloApiGatewayLambdaIntegration, {
+      authorizationType: apigateway.AuthorizationType.Cognito,
+      authorizerId: authorizer.authorizerId,
+    })
+    addCorsOptions(hello)
+  }
 }
 
-const app = new cdk.App();
-
-new myStaticWebsiteStack(app, 'MyStaticSite', { env: { region: 'us-west-2' } });
-
-app.run();
+const app = new cdk.App()
+new LambdaFunctionStack(app, 'HelloWorld')
+app.synth()
