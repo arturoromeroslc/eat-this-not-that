367adccb72a713465d62655e6725166c20d3cd52
Arturo Romero <artromero801@gmail.com>
2019-05-23 00:14:37 -0700

    add prettier

--- packages/infrastructure/index.ts
+++ packages/infrastructure/index.ts
@@ -1,22 +1,24 @@
 #!/usr/bin/env node
-import cdk = require('@aws-cdk/cdk');
-import s3 = require('@aws-cdk/aws-s3');
+import cdk = require('@aws-cdk/cdk')
+import s3 = require('@aws-cdk/aws-s3')
 import iam = require('@aws-cdk/aws-iam')
 
 class myStaticWebsiteStack extends cdk.Stack {
-    constructor(parent: cdk.App, name: string, props: cdk.StackProps) {
-        super(parent, name, props)
+  constructor(parent: cdk.App, name: string, props: cdk.StackProps) {
+    super(parent, name, props)
 
-        const bucket = new s3.Bucket(this, 'myFirstBucket');
-        bucket.addToResourcePolicy(new iam.PolicyStatement()
-            .addActions('s3:GetObject')
-            .addResources(bucket.arnForObjects('file.txt'))
-            .addAccountRootPrincipal());
-    }
+    const bucket = new s3.Bucket(this, 'myFirstBucket')
+    bucket.addToResourcePolicy(
+      new iam.PolicyStatement()
+        .addActions('s3:GetObject')
+        .addResources(bucket.arnForObjects('file.txt'))
+        .addAccountRootPrincipal(),
+    )
+  }
 }
 
-const app = new cdk.App();
+const app = new cdk.App()
 
-new myStaticWebsiteStack(app, 'MyStaticSite', { env: { region: 'us-west-2' } });
+new myStaticWebsiteStack(app, 'MyStaticSite', { env: { region: 'us-west-2' } })
 
-app.run();
+app.run()
